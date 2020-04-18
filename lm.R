7ffc8d188010021e9b621c86a5cda3a3da221196
Miki-Mal <mioodrlus0133@gmail.com>
2020-04-18 19:09:50 +0200

    bug fix

--- lm.R
+++ lm.R
@@ -20,24 +20,26 @@ train_lm <- lm( x~., train)
 sum_train_lm <- summary( train_lm)
 
 # p.val < 0.05
-important_col_bool <- sum_train_lm$coefficients[-1,4] < 0.05
+#important_col_bool <- sum_train_lm$coefficients[-1,4] < 0.05
 #plus label
-important_col_bool <- c( important_col_bool, TRUE)
-train_cut <- train[,important_col_bool]
+#important_col_bool <- c( important_col_bool, TRUE)
+#train_cut <- train[,important_col_bool]
 
 #train on cuted
-train_cut_lm <- lm( x~., train_cut)
-summary(train_cut_lm)
+#train_cut_lm <- lm( x~., train_cut)
+#summary(train_cut_lm)
 
 #predict and plot values
-pred_train_cut <- predict(  train_cut_lm, test[,important_col_bool])
-plot( sort(pred_train_cut))
+#pred_train_cut <- predict(  train_cut_lm, x_test[,important_col_bool])
+#plot( sort(pred_train_cut))
 
 #looking for best cut
 #library(ROCR)
 #pred <- prediction( pred_train_cut, test$x)
 #perf <- performance(pred,"tpr","fpr")
 #plot( perf,colorize=TRUE)
+
+pred_train_cut <- predict(  train_lm, x_test)
 
 #metric
 accuracy <- function( table_in){
@@ -47,7 +49,7 @@ accuracy <- function( table_in){
 acc <- rep(0, 21)
 
 for( i in 0:20){
-  predicted_lab <- ifelse( pred_train_cut > i*0.05, 1, 0)
+  predicted_lab <- ifelse( pred_train > i*0.05, 1, 0)
   acc[i] <- accuracy( table( test$x, predicted_lab))
 }
 
@@ -60,6 +62,5 @@ predicted_lab <- ifelse( pred_train_cut > best_split, 1, 0)
 tab <-table( test$x, predicted_lab)
 accuracy( tab)
 tab
-
 
 
